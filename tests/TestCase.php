62543a4eaa37ee59b6e0d7293428908088c5143e
Ruud Schaaphuizen <r.schaaphuizen@sqits.nl>
2019-04-12 11:23:35 +0200

    removed the mysql option for the unit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,24 +15,6 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         return ['Sqits\UserStamps\UserStampsServiceProvider'];
     }
 
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.connections.mysql', [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'testing'),
-            'username' => env('DB_USERNAME', 'travis'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ]);
-    }
-
     protected function setUp(): void
     {
         parent::setUp();
